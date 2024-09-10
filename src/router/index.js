6e65f89080d8d642263b3d0cae478ccdaf383949
rosieomusic <rosieomusic@gmail.com>
2024-09-09 21:02:09 -0400

    trying webhashhistory to fix home page loading issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter as createRouter, createWebHistory } from 'vue-router'
+import { createRouter as createRouter, createWebHashHistory } from 'vue-router'
 import { useStore } from 'vuex'
 
 // Import components
@@ -21,7 +21,7 @@ import DetailsView from "../views/DetailsView.vue";
  */
 const routes = [
   {
-    path: '/home',
+    path: '/',
     name: 'home',
     component: HomeView,
     meta: {
@@ -81,7 +81,7 @@ const routes = [
 
 // Create the router
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: routes
 });
 
