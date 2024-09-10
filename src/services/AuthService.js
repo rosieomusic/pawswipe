870158d0c7ecce1446615319f32d051b27269a22
rosieomusic <rosieomusic@gmail.com>
2024-09-09 20:39:35 -0400

    added the api call directly in the services

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 // axios.create() not needed, baseURL set in src/main.js
 
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,  // Use the API base URL from your .env file
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
 export default {
 
   login(user) {
