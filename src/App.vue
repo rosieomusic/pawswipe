51c074adfa67a162e5c9d0fc42e7913a5163592a
rosieomusic <rosieomusic@gmail.com>
2025-05-07 13:44:42 -0400

    working on the mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,70 @@
 <template>
-  <div id="paw-swipe-app">
-    <AppHeader />
-    <main>
-      <router-view />
-    </main>
-    <AppFooter />
-  </div>
+	<div id="paw-swipe-app">
+		<AppHeader />
+		<main>
+			<router-view />
+		</main>
+		<AppFooter />
+	</div>
 </template>
 
 <script>
-import AppFooter from "./components/AppFooter.vue";
-import AppHeader from "./components/AppHeader.vue";
+	import AppFooter from './components/AppFooter.vue';
+	import AppHeader from './components/AppHeader.vue';
 
-// Anywhere in your Vue application
+	// Anywhere in your Vue application
 
-
-
-
-export default {
-  components: {
-    AppHeader,
-    AppFooter
-},
-};
-
+	export default {
+		components: {
+			AppHeader,
+			AppFooter,
+		},
+	};
 </script>
 
 <style scoped>
+	#paw-swipe-app {
+		width: 100vw;
+		height: 100;
+		display: grid;
+		grid-template-rows: auto auto auto;
+		grid-template-areas:
+			'header'
+			'main'
+			'footer';
+		background-color: var(--main_bg);
+	}
 
+	#app-header {
+		grid-area: header;
+		position: relative;
+		width: 100vw;
+	}
 
-#paw-swipe-app{
-  width: 100vw;
-  height: 100;
-  display: grid;
-  grid-template-rows: auto auto auto;
-  grid-template-areas:
-    "header"
-    "main"
-    "footer";
-  background-color: var(--main_bg);
+	#app-main {
+		grid-area: main;
+		width: 100vw;
+		height: 100vh;
+		margin-bottom: 5%;
+	}
 
-}
+	#app-footer {
+		grid-area: footer;
+		position: fixed;
+		width: 100%;
+		padding: 10px;
+		bottom: 0;
+	}
 
-#app-header {
-  grid-area: header;
-  position: relative;
-  width: 100vw;
-}
-
-#app-main {
-  grid-area: main;
-  width: 100vw;
-  height: 100vh;
-}
-
-#app-footer {
-  grid-area: footer;
-  position: fixed;
-  width: 100%;
-  padding: 10px;
-  bottom: 0; 
-}
-
-/* Small devices (portrait tablets and large phones, 600px and up) */
-@media only screen and (max-width: 425px) {
-  #paw-swipe-app {
-    grid-template-columns: 1fr;
-    grid-template-rows: auto;
-    grid-template-areas:
-      "header"
-      "main"
-      "footer";
-  }
-  
-
-
-
- 
-}
-
+	/* Small devices (portrait tablets and large phones, 600px and up) */
+	@media only screen and (max-width: 425px) {
+		#paw-swipe-app {
+			grid-template-columns: 1fr;
+			grid-template-rows: auto;
+			grid-template-areas:
+				'header'
+				'main'
+				'footer';
+		}
+	}
 </style>
--- src/components/AnimalCards.vue
+++ src/components/AnimalCards.vue
@@ -1,108 +1,118 @@
 <template>
-    <div id="cards">
-        <div v-for="(eachAnimal, index) in animalData" v-bind:key="eachAnimal.animalId" class="card">
-            <i v-if="$store.getters.isAuthenticated && !$store.getters.isAdmin"
-                v-bind:class="selectedHearts[index] ? 'fa-solid fa-heart' : 'fa-regular fa-heart'"
-                v-on:click="toggleHeart(index)" id="heart">
-            </i>
-            <div id="image">
-                <img v-on:click="handleClick(eachAnimal.animalId)" 
-                v-bind:src="eachAnimal.imageUrl" 
-                alt="beautiful animal">
-            </div>
-            <div id="name">
-                <h1>{{ eachAnimal.animalName }}</h1>
-            </div>
-        </div>
-    </div>
+	<div id="cards">
+		<div
+			v-for="(eachAnimal, index) in animalData"
+			v-bind:key="eachAnimal.animalId"
+			class="card"
+		>
+			<i
+				v-if="$store.getters.isAuthenticated && !$store.getters.isAdmin"
+				v-bind:class="
+					selectedHearts[index] ? 'fa-solid fa-heart' : 'fa-regular fa-heart'
+				"
+				v-on:click="toggleHeart(index)"
+				id="heart"
+			>
+			</i>
+			<div id="image">
+				<img
+					v-on:click="handleClick(eachAnimal.animalId)"
+					v-bind:src="eachAnimal.imageUrl"
+					alt="beautiful animal"
+				/>
+			</div>
+			<div id="name">
+				<h1>{{ eachAnimal.animalName }}</h1>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-    props: ['animalData'],
-    data() {
-        return {
-            selectedFilter: '',
-            selectedHearts: Array(this.animalData.length).fill(false),
-        };
-    },
-    methods: {
-        toggleFilter(filter) {
-            this.selectedFilter = this.selectedFilter === filter ? '' : filter;
-        },
-        toggleHeart(index) {
-            this.selectedHearts[index] = !this.selectedHearts[index];
-        },
-        handleClick(animalId) {
-            this.$router.push({ name: 'details', params: { animalId: animalId } })
-        }
-
-    }
-};
+	export default {
+		props: ['animalData'],
+		data() {
+			return {
+				selectedFilter: '',
+				selectedHearts: Array(this.animalData.length).fill(false),
+			};
+		},
+		methods: {
+			toggleFilter(filter) {
+				this.selectedFilter = this.selectedFilter === filter ? '' : filter;
+			},
+			toggleHeart(index) {
+				this.selectedHearts[index] = !this.selectedHearts[index];
+			},
+			handleClick(animalId) {
+				this.$router.push({ name: 'details', params: { animalId: animalId } });
+			},
+		},
+	};
 </script>
 
 <style scoped>
-#heart {
-    font-size: 32px;
-    margin-left: 92%;
-    cursor: pointer;
-}
+	#heart {
+		font-size: 32px;
+		margin-left: 92%;
+		cursor: pointer;
+	}
 
-#cards {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
-    background-color: #B6ABC1;
-    margin-left: 15%;
-    margin-right: 15%;
-    margin-bottom: 10%;
-    height: 100%;
-    transition: 0.5s;
-    justify-content: center;
-}
+	#cards {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+		background-color: #b6abc1;
+		margin-left: 15%;
+		margin-right: 15%;
+		margin-bottom: 10%;
+		height: 100%;
+		transition: 0.5s;
+		justify-content: center;
+	}
 
-.card {
-    height: 350px;
-    width: 300px;
-    background-color: #C0C5CE;
-    border: 1px solid black;
-    border-radius: 10px;
-    padding: 5%;
-    margin: 5%;
-    text-align: center;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	.card {
+		height: 350px;
+		width: 300px;
+		background-color: #c0c5ce;
+		border: 1px solid black;
+		border-radius: 10px;
+		padding: 5%;
+		margin: 5%;
+		text-align: center;
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	}
 
+	.card:hover {
+		cursor: pointer;
+		transform: translateY(-20px);
+	}
 
-}
+	.card img {
+		width: 100%;
+		height: 200px;
+		object-fit: cover;
+		border-radius: 10px;
+		margin-top: 8px;
+		image-rendering: auto;
+	}
 
-.card:hover {
-    cursor: pointer;
-    transform: translateY(-20px);
-}
+	img {
+		object-fit: fill;
+	}
 
-.card img {
-    width: 100%;
-    height: 200px;
-    object-fit: cover;
-    border-radius: 10px;
-    margin-top: 8px;
-    image-rendering: auto;
-}
+	@media only screen and (max-width: 425px) {
+		#cards {
+			grid-template-columns: 1fr;
+			margin-right: 5%;
+			margin-left: 5%;
+			margin-bottom: 10%;
+			overflow: hidden;
+		}
 
-img {
-    object-fit: fill;
-}
-
-
-@media only screen and (max-width: 425px) {
-    #cards {
-        grid-template-columns: 1fr;
-        margin-right: 5%;
-        margin-left: 5%;
-    }
-
-    .card {
-        margin-right: 40px;
-        margin-left: 40px;
-    }
-}</style>
+		.card {
+			margin-right: 40px;
+			margin-left: 40px;
+		}
+	}
+</style>
